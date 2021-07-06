3ef780d0aeda7653b813bfebd439545c007c78de
William Wang <williamwyj1@gmail.com>
2021-07-06 00:06:30 +0000

    updated server file with preliminary app.post route, installed bodyParser to interpret buffer input from the user

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require('body-parser');
+
+function generateRandomString() {
+  let output = '';
+  for (i = 0;i < 6;i++) {
+    output += '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 62)]
+  }
+  return output;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
 
@@ -26,9 +38,18 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+})
+
 app.get('/urls/:shortURL', (req,res) => {
   templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
 
 app.listen(PORT, () => {
